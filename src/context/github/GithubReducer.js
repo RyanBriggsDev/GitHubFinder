d8da3a64c74495f45c2528fe1390a41734ea350c
RyanMatthewBriggs <ryan.briggs248@gmail.com>
2022-07-12 23:06:57 +0100

    added axios to project to replace fetch api. also tidied up actions

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -13,16 +13,11 @@ const GithubReducer = (state, action) => {
                 ...state,
                 loading: true
             }
-        case 'GET_USER':
+        case 'GET_USER_AND_REPOS':
             return {
                 ...state,
-                user: action.payload,
-                loading: false,
-            }
-        case 'GET_USER_REPOS':
-            return {
-                ...state,
-                repos: action.payload,
+                user: action.payload.user,
+                repos: action.payload.repos,
                 loading: false,
             }
         case 'CLEAR_USERS':
